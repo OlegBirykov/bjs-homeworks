8fc2f18b4681c0d9a9bbca145be574181c949a63
OlegBirykov <p_petro75@mail.ru>
2020-03-31 20:55:28 +0300

    Выполнено задание к занятию 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,34 @@
+'use strict';
+
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+  if (animal === undefined)
+    return null;
+  let sound = animal.sound;
+  return sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  if (marks === undefined) 
+    return;
+  if (marks.length === undefined)
+    return;
+  if (marks.length === 0)
+    return 0;
+  
+  let average = 0;
+  for (let i = 0; i < marks.length; i++)
+    average += Number.parseInt(marks[i]);
+  average /= marks.length;
+
+  let roundedAverage = Math.round(average);
+  return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+
+  let now = Date.now();
+  birthday = new Date(birthday).getTime(); // В задании сказано "записать в переменную birthday"
+  let diff = now - birthday;
+  let age = diff / 31557600000; // 1000 * 60 * 60 * 24 * 365,25
+  return age > 18;
 }
